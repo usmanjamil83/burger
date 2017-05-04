a47cd8884d1121a16fc2e9d2f4137e778e1c288b
usmanjamil83 <usmanjamil83@hotmail.com>
2017-05-04 08:48:20 -0400

    worked on files

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	Burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN NOT NULL DEFAULT false,
+	devoured BOOLEAN DEFAULT false,
 	date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
