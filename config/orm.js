38ad9da1f40b99499a8de1ed8f784fe34ccadaa0
usmanjamil83 <usmanjamil83@hotmail.com>
2017-05-04 08:19:52 -0400

    worked on files

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Object for all our SQL statement functions.
+var orm = {
+  selectAll:
+  insertOne:
+  updateOne:
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
